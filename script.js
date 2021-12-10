bbef534d42e630becf48cf2fbecf2b1f62acfd86
Ruslan <sich92@yandex.ru>
2021-12-10 16:11:28 +0300

    Refactored Factory functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const Player = function(name,sign){
+const Player = function(name,sign,isAI){
     
     getSign = function(){
         return sign
@@ -9,13 +9,16 @@ const Player = function(name,sign){
     setName = function(newName){
         name = newName
     }
+    getIsAI = function(){
+        return isAI
+    }
 
-    return {getSign,getName,setName}
+    return {getSign,getName,setName,getIsAI}
 
 }
 
-const AI = function (name,sign){
-    const prototype = Player(name,sign)
+const AI = function (name,sign,isAI){
+    const prototype = Player(name,sign,isAI)
 
     const _getPossibleStates = function(board){
         let possibleStates = [];
@@ -57,11 +60,9 @@ const AI = function (name,sign){
             let score
             if(result === sign){
                 score = 100-depth
-                //console.log(max)
             }
             if(result === nextSign(sign)){
                 score = -100-depth
-                //console.log(max)
             }
             if(result === "Draw"){
                 score = 0;
@@ -82,7 +83,6 @@ const AI = function (name,sign){
             delete board[state]
         })
         if(depth === 0){
-            console.log(scores)
             
             let max = Math.max(...scores)
             let indexes = []
@@ -134,7 +134,6 @@ const AI = function (name,sign){
 
     const getMove = function (board){
         getBestMove(board,0);
-        console.log(bestMoves)
         if(bestMoves.length === 1){
             return bestMoves[0]
         }
@@ -146,82 +145,12 @@ const AI = function (name,sign){
     return Object.assign({}, prototype,{getRandomMove,getMove})
 }
 
-// const View = (function(){
-      
-// })()
-
 const Gameboard = (function(){
-    const board = document.querySelector(".board")
-    const gameResult = document.querySelector(".game-result")
-    const startGameBtn = document.getElementById("start-game")
-    const player1Input = document.getElementById("player1")
-    const player2Input = document.getElementById("player2")
-    const player1Name = document.querySelector(".player1-name")
-    const player2Name = document.querySelector(".player2-name")
-    const gameStatus = document.querySelector(".play-game")
-    const player1HumanBtn = document.getElementById('player-1-human')
-    const player2HumanBtn = document.getElementById('player-2-human')
-    const player1ComputerBtn = document.getElementById('player-1-computer')
-    const player2ComputerBtn = document.getElementById('player-2-computer')
+    
     let boardState = {}
 
     const Players = []
     let currentPlayer = 0
-
-
-    const _renderBoard = function(){
-        board.innerHTML = ''
-        for(let i=0; i<9; i++){
-            const cell = document.createElement('div')
-            cell.classList.add("cell")
-            cell.setAttribute("data-cell",i)
-            cell.textContent = boardState[i]
-            board.appendChild(cell)
-        }
-    }
-
-    const round = function(){
-        
-        if(Players[currentPlayer].getRandomMove){
-            let move = Players[currentPlayer].getMove(boardState)
-            aiMove(move)
-        }
-        else{
-            return
-        }
-        _renderBoard()
-        let result = winnerCheck();
-            if(result){
-                gameResultRender(result)
-                lockGameBoard();
-            }
-            else{
-                changePlayer()
-                _renderGameStatus()
-                round()
-            }
-        
-    }
-
-    const addMark = function(event){
-        if(event.target.classList[0] === "cell"){
-            let cellNumber = event.target.getAttribute("data-cell")
-            if(!boardState[cellNumber]){
-                boardState[cellNumber] = Players[currentPlayer].getSign()
-                _renderBoard()
-                let result = winnerCheck();
-                if(result){
-                    gameResultRender(result)
-                    lockGameBoard();
-                }
-                else{
-                    changePlayer()
-                    _renderGameStatus()
-                    round()
-                }
-            }
-        }
-    }
 
     const winnerCheck = function(){
         let sign = Players[currentPlayer].getSign()
@@ -245,10 +174,165 @@ const Gameboard = (function(){
         currentPlayer = (currentPlayer === 0) ? 1 : 0
     }
 
+    const clearBoard = function(){
+        boardState = {}
+        currentPlayer = 0;
+    }
+
+    const getBoardState = function(){
+        return boardState
+    }
+
+    const addPlayer = function(Player){
+        Players.push(Player)
+    }
+
+    const getCurrentPlayer = function(){
+        return Players[currentPlayer]
+    }
+
+    const AIPlay = function(){
+        if(Players[currentPlayer].getIsAI()){
+            let move = Players[currentPlayer].getMove(boardState)
+            boardState[move] = Players[currentPlayer].getSign()
+            return true
+        }
+        else{
+            return false
+        }
+    }
+
+    const getCell = function(i){
+        return boardState[i]
+    }
+
+    const setCell = function(i){
+        boardState[i] = Players[currentPlayer].getSign()
+    }
+
+    return {getBoardState, winnerCheck, clearBoard, addPlayer, getCurrentPlayer, changePlayer, AIPlay, getCell, setCell}
+
+    
+})()
+
+const View = (function(){
+    // DOM Elements
+    const board = document.querySelector(".board")
+    const gameResult = document.querySelector(".game-result")
+    const startGameBtn = document.getElementById("start-game")
+    const player1Input = document.getElementById("player1")
+    const player2Input = document.getElementById("player2")
+    const player1Name = document.querySelector(".player1-name")
+    const player2Name = document.querySelector(".player2-name")
+    const gameStatus = document.querySelector(".play-game")
+    const player1HumanBtn = document.getElementById('player-1-human')
+    const player2HumanBtn = document.getElementById('player-2-human')
+    const player1ComputerBtn = document.getElementById('player-1-computer')
+    const player2ComputerBtn = document.getElementById('player-2-computer')
+
+    const _renderBoard = function(){
+        board.innerHTML = ''
+        for(let i=0; i<9; i++){
+            const cell = document.createElement('div')
+            cell.classList.add("cell")
+            cell.setAttribute("data-cell",i)
+            cell.textContent = Gameboard.getBoardState()[i]
+            board.appendChild(cell)
+        }
+    }
+
+    const init = function(){  
+        Gameboard.clearBoard()      
+        gameResult.textContent = ''
+        createPlayer();
+        player1Name.textContent = player1Input.value || "Player 1"
+        player2Name.textContent = player2Input.value || "Player 2"
+        _renderGameStatus() 
+        player1Input.style.display = "none"
+        player2Input.style.display = "none"
+        player1HumanBtn.style.display = "none"
+        player2HumanBtn.style.display = "none"
+        player1ComputerBtn.style.display = "none"
+        player2ComputerBtn.style.display = "none"
+        _renderBoard()
+        board.addEventListener('click',addMark)
+        play()      
+
+    }
+
+    const addMark = function(event){
+        if(event.target.classList[0] === "cell"){
+            let cellNumber = event.target.getAttribute("data-cell")
+            if(!Gameboard.getCell(cellNumber)){
+                Gameboard.setCell(cellNumber)
+                play()
+                _renderBoard()
+                let result = Gameboard.winnerCheck();
+                if(result){
+                    gameResultRender(result)
+                    lockGameBoard();
+                }
+                else{
+                    Gameboard.changePlayer()
+                    _renderGameStatus()
+                    if(Gameboard.getCurrentPlayer().getIsAI()){     
+                        play()
+                    }
+                    }
+                
+            }
+        }
+    }
+
+    const play = function(){
+        if(Gameboard.getCurrentPlayer().getIsAI()){                
+            Gameboard.AIPlay()
+            _renderBoard()
+        let result = Gameboard.winnerCheck();
+        if(result){
+            gameResultRender(result)
+            lockGameBoard();
+        }
+        else{
+            Gameboard.changePlayer()
+            _renderGameStatus()
+            play()
+        }
+        }
+        else{
+            return
+        }    
+        
+        
+    }
+
+    const createPlayer = function(){
+        let Player1
+        let Player2
+        if(player1HumanBtn.disabled){
+            Player1 = Player(player1Input.value || "Player 1", "X", false)
+        }
+        else{
+            Player1 = AI(player1Input.value || "Player 1", "X", true)
+        }
+        if(player2HumanBtn.disabled){
+            Player2 = Player(player2Input.value || "Player 2", "0", false)
+        }
+        else{
+            Player2 = AI(player2Input.value || "Player 2", "0", true)
+        }
+        Gameboard.addPlayer(Player1)
+        Gameboard.addPlayer(Player2)
+    }
+
+    const _renderGameStatus = function(){
+        gameStatus.textContent = `${Gameboard.getCurrentPlayer().getName()} - ${Gameboard.getCurrentPlayer().getSign()}. Your turn!`
+    }
+
     const gameResultRender = function(result){
         let template = ''
         if(result === "X" || result === "0"){
-            template = `The winner is ${Players[currentPlayer].getName()}`
+            template = `The winner is ${Gameboard.getCurrentPlayer().getName()}`
         }
         else{
             template = "Draw"
@@ -261,62 +345,7 @@ const Gameboard = (function(){
         board.removeEventListener('click',addMark)
     }
 
-    const init = function(){
-        boardState = {}            
-        currentPlayer = 0;         
-        gameResult.textContent = ''     
-        checkPlayerOrAI();
-        player1Name.textContent = Players[0].getName()
-        player2Name.textContent = Players[1].getName()
-        _renderGameStatus() 
-        player1Input.style.display = "none"
-        player2Input.style.display = "none"
-        player1HumanBtn.style.display = "none"
-        player2HumanBtn.style.display = "none"
-        player1ComputerBtn.style.display = "none"
-        player2ComputerBtn.style.display = "none"
-        _renderBoard()
-        board.addEventListener('click',addMark)
-        round()      
-
-    }
-
-    const checkPlayerOrAI = function(){
-        let Player1
-        let Player2
-        if(player1HumanBtn.disabled){
-            Player1 = Player(player1Input.value || "Player 1", "X")
-        }
-        else{
-            Player1 = AI(player1Input.value || "Player 1", "X")
-        }
-        if(player2HumanBtn.disabled){
-            Player2 = Player(player2Input.value || "Player 2", "0")
-        }
-        else{
-            Player2 = AI(player2Input.value || "Player 2", "0")
-        }
-        Players.push(Player1,Player2)
-    }
-
-    const _renderGameStatus = function(){
-        gameStatus.textContent = `${Players[currentPlayer].getName()} - ${Players[currentPlayer].getSign()}. Your turn!`
-    }
-
-    const getBoardState = function(){
-        return boardState
-    }
-
-    const aiMove = function(move){
-        //let move = Players[currentPlayer].getRandomMove()
-        boardState[move] = Players[currentPlayer].getSign()
-        
-    }
-
-    
-    _renderBoard()
-
-    //event Listeners
+    //Event Listeners
     startGameBtn.addEventListener('click',init)
 
     player1HumanBtn.addEventListener('click', () => {
@@ -339,10 +368,13 @@ const Gameboard = (function(){
         player2ComputerBtn.disabled = true
     })
 
-
-    return {getBoardState}
-
+    _renderBoard()
     
 })()
 
-x = AI("Ruslan", "X")
+
+
+// const View = (function(){
+      
+// })()
+
